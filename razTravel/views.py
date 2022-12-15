f5824b9bd28fb33e6433266fdf511dea1d61a103
PamodiRanmal <ranmalmendis8@gmail.com>
2022-12-15 13:41:50 +0530

    displaying offers with if blocks jinja

--- razTravel/views.py
+++ razTravel/views.py
@@ -10,18 +10,24 @@ def index(request):
     dest1.image='destination_1.jpg'
     dest1.description='The City that Never Sleeps'
     dest1.price=790
+    dest1.offer=False
 
     dest2= Destination()
     dest2.name='Colombo'
     dest2.image='destination_2.jpg'
     dest2.description='The capital of Sri Lanka'
     dest2.price=990
+    dest2.offer=True
+
+
 
     dest3= Destination()
     dest3.name='Galle'
     dest3.image='destination_3.jpg'
     dest3.description='Hey it\'s home'
     dest3.price=890
+    dest3.offer=True
+
 
     dests=[dest1,dest2,dest3]
     return render(request,'index.html',{'dests':dests})
