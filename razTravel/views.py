bdc95dba36dbcbc9c0a381f3f4833d31affe9b2e
PamodiRanmal <ranmalmendis8@gmail.com>
2022-12-15 13:33:55 +0530

    displaying the dynamic contents in index.html

--- razTravel/views.py
+++ razTravel/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
+from .models import Destination
 
 # Create your views here.
 
 def index(request):
     # return HttpResponse("Hello world")
-    return render(request,'index.html')
+    dest1= Destination()
+    dest1.name='Mumbai'
+    dest1.image='destination_1.jpg'
+    dest1.description='The City that Never Sleeps'
+    dest1.price=790
+
+    dest2= Destination()
+    dest2.name='Colombo'
+    dest2.image='destination_2.jpg'
+    dest2.description='The capital of Sri Lanka'
+    dest2.price=990
+
+    dest3= Destination()
+    dest3.name='Galle'
+    dest3.image='destination_3.jpg'
+    dest3.description='Hey it\'s home'
+    dest3.price=890
+
+    dests=[dest1,dest2,dest3]
+    return render(request,'index.html',{'dests':dests})
