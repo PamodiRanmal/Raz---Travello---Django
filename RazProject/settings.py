fd3963d66e989c83ab7efbc9110b775c846fd52b
PamodiRanmal <ranmalmendis8@gmail.com>
2022-12-16 18:39:35 +0530

    Creating tables in postgresql with ORM

--- RazProject/settings.py
+++ RazProject/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'razTravel.apps.RaztravelConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,13 @@ WSGI_APPLICATION = 'RazProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'razTravello',
+        'USER':'postgres',
+        'PASSWORD':'root',
+        'HOST':'localhost'
+
+
     }
 }
 
--- makefile
+++ makefile
@@ -1,2 +1,9 @@
 run:
 	python manage.py runserver
+
+migration:
+	python manage.py makemigrations
+	python manage.py sqlmigrate razTravel 0002
+	python manage.py migrate
+	
+
